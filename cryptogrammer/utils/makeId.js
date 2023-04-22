114322fdcce03a14672bff76e5a80202f9c4864f
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-04-22 20:27:55 +0400

    fix: ensure consistent creator names in CreatorCard component

--- cryptogrammer/utils/makeId.js
+++ cryptogrammer/utils/makeId.js
@@ -1,11 +1,26 @@
-// This makeId function is used to generate a random string of characters of a given length. 
-export const makeId = (length) => {
+export const makeId = (length, seed) => {
   let result = '';
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   const charactersLength = characters.length;
 
+  const random = Math.seed(seed);
+
   for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    result += characters.charAt(Math.floor(random() * charactersLength));
   }
   return result;
 }
+
+Math.seed = function(s) {
+  var mask = 0xffffffff;
+  var m_w = (123456789 + s) & mask;
+  var m_z = (987654321 - s) & mask;
+
+  return function() {
+    m_z = (36969 * (m_z & 65535) + (m_z >> 16)) & mask;
+    m_w = (18000 * (m_w & 65535) + (m_w >> 16)) & mask;
+    var result = ((m_z << 16) + m_w) & mask;
+    result /= 4294967296;
+    return result + 0.5;
+  }
+};
