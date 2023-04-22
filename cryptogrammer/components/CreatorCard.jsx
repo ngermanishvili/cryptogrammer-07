fb8ccbf5fbdc6201385e96760902776d0d4e362c
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-04-22 16:53:50 +0400

    Added Creator information to card

--- cryptogrammer/components/CreatorCard.jsx
+++ cryptogrammer/components/CreatorCard.jsx
@@ -5,8 +5,10 @@ import images from "../assets";
 const CreatorCard = ({ rank, creatorImage, creatorName, creatorEths }) => {
   return (
     <div className="min-w-190 minlg:min-w-240 dark:bg-nft-black-3 bg-white border dark:border-nft-black-3 border-nft-gray-1 rounded-3xl flex flex-col p-4 m-4">
-      <div className="">
-        <p>{rank}</p>
+      <div className="w-8 h-8 minlgh:w-7 minlg:h-10 bg-nft-red-violet flexCenter rounded-full">
+        <p className="font-poppins text-white font-semibold minlg-text-lg">
+          {rank}
+        </p>
       </div>
 
       <div className="my-2 flex justify-center ">
@@ -18,12 +20,15 @@ const CreatorCard = ({ rank, creatorImage, creatorName, creatorEths }) => {
           />
 
           <div className="absolute w-4 h-4 minlg:w-7 minlh:h-7 bottom-2 -right-0">
-            <Image
-              src={images.tick}
-              alt="tick"
-            />
+            <Image src={images.tick} alt="tick" />
           </div>
         </div>
+      </div>
+
+
+      <div>
+        <p>{creatorName}</p>
+        <p>{creatorEths.toFixed(2)}</p>
       </div>
     </div>
   );
