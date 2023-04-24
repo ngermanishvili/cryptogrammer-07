24278ae163169b8d1abe73158e9fa9725cc045f5
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-04-25 03:22:06 +0400

    added codemod

--- cryptogrammer/components/NFTCard.jsx
+++ cryptogrammer/components/NFTCard.jsx
@@ -6,35 +6,36 @@ import images from "../assets";
 const NFTCard = ({ nft }) => {
   return (
     <Link href={{ pathname: "/nft-details", query: { nft } }}>
-      <div className="flex-1 min-w-215 max-w-max xs:max-w-none sm:w-full sm:min-w-155 minmd:min-w-256 minlg:min-w-327 dark:bg-nft-black-3 bg-white rounded-2xl p-4 m-4 minlg:m-8 sm:my-2 sm:mx-2">
-        {nft.name}
+      <div
+        key={nft.seller}
+        className="flex-1 min-w-215 max-w-max xs:max-w-none sm:w-full sm:min-w-155 minmd:min-w-256 minlg:min-w-327 dark:bg-nft-black-3 bg-white rounded-2xl p-4 m-4 minlg:m-8 sm:my-2 sm:mx-2 cursor-pointer shadow-md"
+      >
+        <div className="relative w-full h-52 sm:h-36 xs:h-56 minmd:h-60 minlg:h-300 rounded-2xl overflow-hidden">
+          <Image
+            layout="fill"
+            objectFit="cover"
+            className="object-fill layout-auto"
+            src={nft.image || images[`nft${nft.i}`]}
+            alt={`nft${nft.i}`}
+          />
+        </div>
+        <div className="mt-3 flex flex-col">
+          <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm minlg:text-xl">
+            {nft.name}
+          </p>
+          <div className="flexBetween mt- minlg:mt-3 flex-row xs:flex-col xs:items-start xs:mt-3 ">
+            <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm minlg:text-xs">
+              {nft.price}
+              <span key={nft.id} className="normal">
+                ETH
+              </span>
+            </p>
+            <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm minlg:text-lg">{`Jane Doe`}</p>
+          </div>
+        </div>
       </div>
     </Link>
   );
 };
 
 export default NFTCard;
-
-{
-  /* <div className='mt-10 '>
-<div className='flexBetween mx-4 xs:mx-0 minlg:mx-8 sm:flex-col sm:items-start'>
-  <h1 className='flex-1 font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold sm:mb-'>
-    Hot bids
-  </h1>
-  <div>
-    Searchbar
-  </div>
-  <div className='mt-3 w-full flex flex-wrap justify-start md:justify-center'>
-    {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
-      <NFTCard
-        key={`nft-${i}`}
-        nft={{
-          i,
-          name: `Nifty Nft ${i}`,
-          seller: `0x${makeId(3)}...${makeId(4)}`,
-          owner: `0x${makeId(3)}...${makeId(4)}`,
-          description 
-        }}
-      />
-    ))} */
-}
