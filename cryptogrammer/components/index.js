91b990ec5b2656981a6968acfeef045b2e0c99f6
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-04-25 00:19:39 +0400

    Adding NFT cards

---
+++ cryptogrammer/components/index.js
@@ -0,0 +1,8 @@
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
+import Button from '@/components/Button'
+import Banner from '@/components/Banner'
+import CreatorCard from '@/components/CreatorCard'
+import NFTCard from './NFTCard'
+
+export { Navbar, Footer, Button, Banner, CreatorCard, NFTCard } 
