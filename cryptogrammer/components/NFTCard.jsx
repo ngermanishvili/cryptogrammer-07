5b88f9125b908ad6702aeea2151f6d5876e64e0e
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-04-25 15:47:32 +0400

    nft card Updated

--- cryptogrammer/components/NFTCard.jsx
+++ cryptogrammer/components/NFTCard.jsx
@@ -5,7 +5,7 @@ import images from "../assets";
 
 const NFTCard = ({ nft }) => {
   return (
-    <Link href={{ pathname: "/nft-details", query: { nft } }}>
+    <Link href={{ pathname: "/nft-details", query:  nft  }}>
       <div
         key={nft.seller}
         className="flex-1 min-w-215 max-w-max xs:max-w-none sm:w-full sm:min-w-155 minmd:min-w-256 minlg:min-w-327 dark:bg-nft-black-3 bg-white rounded-2xl p-4 m-4 minlg:m-8 sm:my-2 sm:mx-2 cursor-pointer shadow-md"
