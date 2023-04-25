4ab27a0d174000d0d2e5ee23affecdb39bf3b37e
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-04-25 15:49:48 +0400

    component index.js updated

--- cryptogrammer/components/index.js
+++ cryptogrammer/components/index.js
@@ -4,5 +4,6 @@ import Button from '@/components/Button'
 import Banner from '@/components/Banner'
 import CreatorCard from '@/components/CreatorCard'
 import NFTCard from './NFTCard'
+import Input from './Input'
 
-export { Navbar, Footer, Button, Banner, CreatorCard, NFTCard } 
+export { Navbar, Footer, Button, Banner, CreatorCard, NFTCard, Input } 
