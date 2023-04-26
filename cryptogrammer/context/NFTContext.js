3f4125e971986c4bb70517fb91bb109d9f999344
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-04-26 16:21:17 +0400

    add ipfs configuration

--- cryptogrammer/context/NFTContext.js
+++ cryptogrammer/context/NFTContext.js
@@ -1,62 +1,77 @@
-import React, { useState, useEffect } from 'react';
-import Web3Modal from 'web3modal';
-import { ethers } from 'ethers';
-import axios from 'axios';
-import {create as ipfsHttpClient} from 'ipfs-http-client';
+    import React, { useState, useEffect } from 'react';
+    import Web3Modal from 'web3modal';
+    import { ethers } from 'ethers';
+    import axios from 'axios';
+    import { create } from 'ipfs-http-client'
 
-import { MarketAddress, MarketAddressABI } from './constants';
+    import { MarketAddress, MarketAddressABI } from './constants';
 
-const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0');
+    const client = create({
+        host: 'ipfs.infura.io',
+        port: 5001,
+        protocol: 'https',
+        headers: {
+            authorization: 'Basic ' + Buffer.from('2OxhaN6gjV1EHhaCrXbDX45NQx5' + ':' + '536d703a92ee7178f15bcee947b0d6e6').toString('base64')
+        },
+        cors: true
+    });
 
-export const NFTContext = React.createContext();
 
-export const NFTProvider = ({ children }) => {
-    const [currentAccount, setCurrentAccount] = useState();
-    const nftCurrency = "NGC";
+    export const NFTContext = React.createContext();
 
-    const checkIfWalletIsConnected = async () => {
-        if (!window.ethereum) {
-            return alert('Please install MetaMask');
+    export const NFTProvider = ({ children }) => {
+        const [currentAccount, setCurrentAccount] = useState();
+        const nftCurrency = "NGC";
+
+        const checkIfWalletIsConnected = async () => {
+            if (!window.ethereum) {
+                return alert('Please install MetaMask');
+            }
+
+            const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+
+
+
+            if (accounts.length) {
+                const account = accounts[0];
+                setCurrentAccount(account);
+            } else {
+                console.log('No authorized account found');
+            }
         }
 
-        const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+        useEffect(() => {
+            checkIfWalletIsConnected();
+        }, [])
 
+        const connectWallet = async () => {
+            if (!window.ethereum) {
+                return alert('Please install MetaMask');
+            }
+            const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
 
+            setCurrentAccount(accounts[0]);
 
-        if (accounts.length) {
-            const account = accounts[0];
-            setCurrentAccount(account);
-        } else {
-            console.log('No authorized account found');
+            window.location.reload();
         }
+        const uploadToIPFS = async (file) => {
+            try {
+                const added = await client.add({ content: file });
+                const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+                return Promise.resolve(url);
+            } catch (error) {
+                console.log('Error uploading file to IPFS: ', error);
+                return Promise.reject(error);
+            }
+        }
+
+
+
+        return (
+            // This value is passed down to any components that are consuming this context for example in the NFT component
+            <NFTContext.Provider value={{ nftCurrency, connectWallet, currentAccount, uploadToIPFS }}>
+                {children}
+            </NFTContext.Provider>
+        )
     }
-
-    useEffect(() => {
-        checkIfWalletIsConnected();
-    }, [])
-
-    const connectWallet = async () => {
-        if (!window.ethereum) {
-            return alert('Please install MetaMask');
-        }
-        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-
-        setCurrentAccount(accounts[0]);
-
-        window.location.reload();
-    }
-    const uplpadToIpfs = async (file, setFileUrl) => {
-        try{
-
-        } catch (error) {
-
-        }
-    }
-    return (
-        // This value is passed down to any components that are consuming this context for example in the NFT component
-        <NFTContext.Provider value={{ nftCurrency, connectWallet, currentAccount }}>
-            {children}
-        </NFTContext.Provider>
-    )
-}
 
